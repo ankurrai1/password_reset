0aaaebf3abdaf98e9876dcc7114ebdfbaaffa257
ankur <rai.ankur0007@gmail.com>
2019-04-21 12:16:57 +0530

    database and server connection cofigured and authentication is done tother with session

--- api/app.js
+++ api/app.js
@@ -1,15 +1,70 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const logger = require("morgan");
 const path = require('path');
+const logger = require("morgan"); // to log user requested url on server
+const dotenv = require("dotenv"); // to config envirnment
+const express = require("express");
+const mongoose = require('mongoose');
+const mongo = require("connect-mongo")
+const bodyParser = require("body-parser");
 
+// for security these this are
+const expressValidator = require("express-validator");
+const  session = require('express-session');
+const compression = require("compression");
+const helmet = require("helmet");
+const lusca = require("lusca");
+
+const cors = require("cors"); // for cross-origin resource shearing at http.
+const bluebird = require("bluebird"); // used for creatin promise
+
+// var Pusher = require('pusher'); will not use it here it is used to real time pushing and need account
 
 const app = express();
 dotenv.config({path: ".env"});
+app.set("port", process.env.PORT);
 
+
+// MONGO SETUP
+const MongoStore = mongo(session);
+
+const mongoUrl = process.env.TEST ? process.env.MONGO_TEST : process.env.MONGO_URI;
+
+(mongoose).Promise = bluebird;
+mongoose.connect(mongoUrl, {useNewUrlParser: true})
+        .then(() => console.log("Connected to Mongo DB"))
+        .catch(err =>console.log("MongoDB connection error. Please make sure MongoDB is running. " + err));
+
+app.use(helmet());
+app.use(compression());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressValidator());
+app.use(session({
+    resave: true,
+    saveUninitialized: true,
+    secret: process.env.SESSION_SECRET,
+    store: new MongoStore({
+        url: mongoUrl,
+        autoReconnect: true
+    })
+}));
+app.use(lusca.xframe("SAMEORIGIN"));
+app.use(lusca.xssProtection(true));
+
+app.use(cors());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Expose-Headers", "Authorization");
+    next();
+});
 app.use(logger("dev"));
-app.set("port", process.env.PORT || 9000);
-app.use(express.static(__dirname + "public"));
+
+
+// for testing if the server is running
+app.get('/', function(req, res){ 
+    res.send('<html>all is well...</html>');
+});
+
 
 
 module.exports = app;
--- api/test/app_test.js
+++ api/test/app_test.js
@@ -3,7 +3,7 @@ let assert = require("chai").assert;
 let app = require("../app.js");
 
 
-describe("app", () => {
+describe("APP", () => {
     beforeEach(function () {
         handleError = function (done) {
             return function (err, res) {
@@ -15,11 +15,21 @@ describe("app", () => {
         };
     });
     describe("GET /bad", () => {
-        it("resonds with 404", done => {
+        it("it should resonds with 404", done => {
             request(app)
                 .get("/bad")
                 .expect(404)
                 .end(handleError(done));
         });
+        it("should resonds with 200", done => {
+            request(app)
+                .get("/")
+                .expect(200)
+                .expect("Content-Type", "text/html; charset=utf-8")
+                .expect(/all is well/)
+                .end(handleError(done));
+        });
     });
+    // describe("GET /", () => {
+    // });
 });
